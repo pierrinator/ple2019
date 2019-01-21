fcbf8464d0635e9e6c58dfa91e8912d628e3b25a
pierrinator <plorson3@gmail.com>
2019-01-21 11:02:54 +0100

    début de routes leaflet

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var L = require('leaflet');
+var express = require('express');
 
+var app = express();
 var map = L.map('map');
+
+app.use('/:z/:x/:y', imgController);
 
 map.setView([47, 13], 7);
 
 var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 }).addTo(map);
+
